8605e11fdfd7620494758303a20ed83332852601
Levi Francis <levifrancis15@gmail.com>
2024-09-28 12:38:52 +0100

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Start from './components/Start'
 import QuizBox from './components/QuizBox'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Routes>
         <Route path="/" element={<Start />} />
         <Route path='/start' element={<QuizBox />} />
-        {/* <Route path="/extension" element={<Extension />} />
-        <Route path="/testnet" element={<Navigate to={"/dashboard"} />} />
-        <Route path="/contact" element={<Navigate to={"/"} />} />
-        <Route path="/faucet" element={<Faucet />} /> */}
       </Routes>
     </>
   )
